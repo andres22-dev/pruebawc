6eefda15f41ae1af18ea5c8d510752f107783c4d
andres22 <afpinchao8@misena.edu.co>
2021-07-24 17:40:50 -0500

    creando carpeta de componentes y agregando assets para la maquetacion de los componentes

---
+++ src/components/LandingPage.js
@@ -0,0 +1,30 @@
+
+import React from 'react';
+
+const LandingPage = () => {
+
+  return (
+    <>
+      <div>
+        <div>
+        <header>
+          <nav>
+            <div>
+              <img src="../../public/assets/BigLogo.svg"></img>
+            </div>
+            <ul>
+              <li>INICIO</li>
+              <li>BENEFICIOS</li>
+              <li>LOGIN</li>
+            </ul>
+          </nav>
+        </header>
+        <div><img src="../../public/assets/Planet.svg"></img></div>
+        </div>
+      </div>
+    </>
+  )
+
+}
+
+export default LandingPage;
