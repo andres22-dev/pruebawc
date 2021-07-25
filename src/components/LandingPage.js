67c28640181ef3a25090273a1936f17d4ba1b01d
andres22 <afpinchao8@misena.edu.co>
2021-07-25 11:53:09 -0500

    arreglando error en grid y  borrando css no necesario

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,12 +3,12 @@ import React from 'react';
 import { LandingContainer } from './styles/LandingPageStyle';
 import BigLogo from '../assets/img/BigLogo.svg';
 import Planet from '../assets/img/Planet.svg';
+import Mockups from '../assets/img/MockupProducts.svg';
 
 const LandingPage = () => {
 
   return (
     <>
-      <LandingContainer>
         <div className="layoutcontainer">
           <div className="layoutcontainer__home">
             <header className="header">
@@ -23,17 +23,18 @@ const LandingPage = () => {
                 </ul>
               </nav>
             </header>
+            
             <div className="planet">
               <img src={Planet} />
-               
+            </div> 
+          </div>
+
+          <div className="layoutcontainer__publicity">
+            <div>
+            <img src={Mockups} />
             </div>
-             <div>
-                <p>Bienvenido a tu Entrevista Tecnica en WACO</p>
-              </div>  
-            
           </div>
         </div>
-      </LandingContainer>
     </>
   )
 
